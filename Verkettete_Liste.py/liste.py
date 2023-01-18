a721e503b87f59833a5308a5b121589dd15152bb
Miguel <62133840+Primtaxx@users.noreply.github.com>
2023-01-18 09:27:13 +0100

    Create liste.py

---
+++ Verkettete_Liste.py/liste.py
@@ -0,0 +1,46 @@
+class ListElement:
+    def __init__(self, obj):
+        self.obj = obj
+        self.nextElem = None
+        
+    def setNextElem(self, nextElem):
+        self.nextElem = nextElem
+        
+    def getNextElem(self):
+        return self.nextElem
+    
+    def getObj(self):
+        return self.obj
+
+class SimpleLinkedList:
+    def __init__(self):
+        self.startElem = ListElement("Head")
+    
+    def getFirstElem(self):
+        return self.startElem
+    
+    def getLastElem(self):
+        start = self.startElem
+        while start.getNextElem() is not None:
+            start = start.getNextElem()
+        return start
+
+    def addLast(self, element):
+        newElem = ListElement(element)
+        lastElem = self.getLastElem()
+        lastElem.setNextElem(newElem)
+
+    def writeList(self):
+        write = self.startElem
+        while write is not None:
+            print(write.getObj())
+            write = write.getNextElem()
+
+if __name__ == '__main__':
+    list = SimpleLinkedList()
+    list.addLast("1")
+    list.addLast("2")
+    list.addLast("3")
+    list.addLast("4")
+    list.addLast("5")
+    list.writeList()
